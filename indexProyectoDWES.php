9493d8bc0313b72992f62adf303f11e5c58b9588
outmane <outmane.bou.1@educa.jcyl.es>
2021-10-15 10:07:39 +0200

    añadir en proyectoDWES

---
+++ indexProyectoDWES.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OUTMANE BOUHOU:ProyectoDWES</title>
+    <link rel="stylesheet" type="text/css" href="webroot/css/main.css">
+    <link rel="stylesheet" type="text/css" href="../webroot/css/proyectoTema3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <div class="w3-container">
+        <div class="w3-bar w3-black">
+            <a href="/index.html" class="w3-bar-item w3-button w3-mobile w3-hover-blue" style="width:33%">Home</a>
+            <a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-blue" style="width:33%">Contact</a>
+            <a href="#" class="w3-bar-item w3-button w3-mobile w3-hover-blue" style="width:33%">About</a>
+        </div>
+        <h2>Bienvenido a la pagina principal de Desarrollo Web en Entorno Servidor </h2>
+
+        <ul id="ulli" class="w3-ul w3-card-4">
+            <p>
+                <li class="w3-display-container">TEMA 1: DESARROLLO WEB EN ENTORNO SERVIDOR</li>
+            </p>
+            <p>
+                <li class="w3-display-container">TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE
+                    DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</li>
+            </p>
+            <p>
+                <li class="w3-display-container"><a style="color: blue;"
+                                                    href="/proyectoTema3/indexProyectoTema3.php">TEMA 3: CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>
+            </p>
+            <p>
+                <li class="w3-display-container">TEMA 4: TÉCNICAS DE ACCESO A DATOS EN PHP</li>
+            </p>
+            <p>
+                <li class="w3-display-container">TEMA 5: DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</li>
+            </p>
+            <p>
+                <li class="w3-display-container">TEMA 6: PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</li>
+            </p>
+            <p>
+                <li class="w3-display-container">TEMA 7: PROGRAMACIÓN DE SERVICIOS WEB</li>
+            </p>
+            <p>
+                <li class="w3-display-container">TEMA 8: DESARROLLO DE APLICACIONES WEB HÍBRIDAS</li>
+            </p>
+
+        </ul>
+        <!--<?php
+highlight_file("test.php");
+?>-->
+    </div>
+    <footer>
+        <p>Copyrights © 2021 OUTMANE BOUHOU. All rights reserved.</p>
+       <a href="https://github.com/outmaneBH/proyectoDAW" target="_blank" ><img id="git" src="../webroot/media/img/git.png" alt="Github"/></a>
+    </footer>
+</body>
+
+</html>
